bb49ea111ef8a4b062e751013a015f61f71b0eb9
Rares Catalin Onescu <raresonescu@gmail.com>
2021-04-21 11:15:18 +0300

    A vertical ScreenSaver

---
+++ src/com/MyFrame.java
@@ -0,0 +1,22 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyPanel panel;
+
+    MyFrame(){
+
+        panel = new MyPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+
+
+}
---
+++ src/com/MyPanel.java
@@ -0,0 +1,40 @@
+package com;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class MyPanel extends JPanel implements ActionListener {
+    final int panelLungime =1900;
+    final int panelInaltime =300;
+    Image imagine;
+    Timer timer;
+    int xVelocity = 30;
+    int x=0;
+    int y=0;
+
+    MyPanel(){
+        this.setPreferredSize(new Dimension(panelLungime,panelInaltime));
+        this.setBackground(Color.white);
+        imagine = new ImageIcon("logoRCO.png").getImage();
+        timer= new Timer(10,this);
+        timer.start();
+    }
+    public void paint (Graphics g){
+
+        super.paint(g);
+
+        Graphics2D g2D = (Graphics2D) g;
+
+        g2D.drawImage(imagine,x,y,null);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (x>=panelLungime-imagine.getWidth(this)|| x<0)
+        {
+            xVelocity=xVelocity * (-1);
+        }
+    x=x+xVelocity;
+    repaint();
+    }
+}
